759be2b75bb9bd4b8f6c574ef7a9b7b9a18b1f49
syahrilariandi18 <syahrilariandi7.gmail.com>
2026-05-21 14:58:36 +0700

    commit

--- resources/views/auth/Login.blade.php
+++ resources/views/auth/Login.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Login - Mini POS Syahril</title>
+    <title>Login - Mini POS</title>
 
     <!-- Custom fonts for this template -->
     <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Latihan 03 Syahril</title>
+    <title>Latihan 03 Login Syahril</title>
 
     <!-- Custom fonts for this template -->
     <link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
